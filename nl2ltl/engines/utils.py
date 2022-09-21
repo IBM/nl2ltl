cb8db303a915113ec47df16544504c676302b791
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2022-09-21 17:11:46 +0200

    fixes
    Signed-off-by: Francesco Fuggitti <francesco.fuggitti@gmail.com>

---
+++ nl2ltl/engines/utils.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""Engines utils."""
+import difflib
+from typing import Callable, Dict, Set
+
+from pylogics.syntax.base import Formula
+
+from nl2ltl.declare.base import TemplateEnum
+
+
+def _get_formulas(name: str, args: Dict[str, float]) -> Set[Formula]:
+    """Instantiate matching formulas based on intent name and entities."""
+    import nl2ltl.declare.declare
+    import nl2ltl.engines.grounding
+
+    class_name_match = difflib.get_close_matches(
+        name, [x.value for x in TemplateEnum], n=1
+    )
+    grounding_map: Dict[str, Callable] = {}
+    for c_name in TemplateEnum:
+        grounding_map[c_name.value] = getattr(
+            nl2ltl.engines.grounding, f"ground_{c_name.value.lower()}"
+        )
+
+    grounding_func: Callable = grounding_map[str(class_name_match[0])]
+    grounded_formulas: Set[Formula] = grounding_func(args)
+    return grounded_formulas
