46a61d94de4fc41adf95615cdfaece7531294330
Francesco Fuggitti <fuggitti@diag.uniroma1.it>
2022-09-22 22:45:45 +0200

    improve tests
    Signed-off-by: Francesco Fuggitti <francesco.fuggitti@gmail.com>

--- tests/test_gpt3.py
+++ tests/test_gpt3.py
@@ -7,7 +7,7 @@ import pytest
 
 from nl2ltl import translate
 from nl2ltl.engines.gpt3.core import GPT3Engine
-from nl2ltl.filters.simple_filters import BasicFilter
+from nl2ltl.filters.simple_filters import BasicFilter, GreedyFilter
 
 from .conftest import UtterancesFixtures
 
@@ -22,9 +22,16 @@ class TestGPT3:
         """
         cls.gpt3_engine = GPT3Engine()
         cls.basic_filter = BasicFilter()
+        cls.greedy_filter = GreedyFilter()
 
     @pytest.mark.parametrize("utterance", UtterancesFixtures.utterances)
-    def test_gpt3_engine(self, utterance):
-        """Test GPT-3 engine for utterances."""
+    def test_gpt3_engine_basic(self, utterance):
+        """Test GPT-3 engine for utterances with basic filter."""
         output = translate(utterance, self.gpt3_engine, self.basic_filter)
+        assert isinstance(output, Dict)
+
+    @pytest.mark.parametrize("utterance", UtterancesFixtures.utterances)
+    def test_gpt3_engine_greedy(self, utterance):
+        """Test GPT-3 engine for utterances with greedy filter."""
+        output = translate(utterance, self.gpt3_engine, self.greedy_filter)
         assert isinstance(output, Dict)
