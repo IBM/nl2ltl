9dbfec0d92eef9a36f925746f65d47b1e9a7c8fe
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2023-06-08 13:41:51 -0400

    add GPT4 files

---
+++ tests/test_gpt4.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for GPT-3 engine."""
+from typing import Dict
+
+import pytest
+
+from nl2ltl import translate
+from nl2ltl.engines.gpt4.core import GPT4Engine
+from nl2ltl.filters.simple_filters import BasicFilter, GreedyFilter
+
+from .conftest import UtterancesFixtures
+
+
+class TestGPT4:
+    """GPT-3 test class."""
+
+    @classmethod
+    def setup_class(cls):
+        """setup any state specific to the execution of the given class (which
+        usually contains tests).
+        """
+        cls.gpt4_engine = GPT4Engine()
+        cls.basic_filter = BasicFilter()
+        cls.greedy_filter = GreedyFilter()
+
+    @pytest.mark.parametrize("utterance", UtterancesFixtures.utterances)
+    def test_gpt4_engine_basic(self, utterance):
+        """Test GPT-3 engine for utterances with basic filter."""
+        output = translate(utterance, self.gpt4_engine, self.basic_filter)
+        assert isinstance(output, Dict)
+
+    @pytest.mark.parametrize("utterance", UtterancesFixtures.utterances)
+    def test_gpt4_engine_greedy(self, utterance):
+        """Test GPT-3 engine for utterances with greedy filter."""
+        output = translate(utterance, self.gpt4_engine, self.greedy_filter)
+        assert isinstance(output, Dict)
