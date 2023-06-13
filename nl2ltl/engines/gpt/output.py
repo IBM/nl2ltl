f67f929bd18c8a3e4d4e8fcb86087d11e55e0cf2
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2023-06-13 12:25:26 -0400

    feat: allow for multiple GPT modes of operations
    Signed-off-by: Francesco Fuggitti <francesco.fuggitti@gmail.com>

--- nl2ltl/engines/gpt/output.py
+++ nl2ltl/engines/gpt/output.py
@@ -7,6 +7,7 @@ from typing import Dict, Match, Set, Tuple, cast
 
 from pylogics.syntax.base import Formula
 
+from nl2ltl.engines.gpt.core import OperationModes
 from nl2ltl.engines.utils import _get_formulas
 from nl2ltl.filters.base import Filter
 
@@ -29,37 +30,62 @@ class _GPTOutputWrapper:
     """A wrapper to the textual output of GPT."""
 
     output: dict
+    mode: str
 
     @property
     def pattern(self) -> str:
         """Get the predicted pattern."""
-        return str(
-            cast(
-                Match, re.search("PATTERN: (.*)\n", self.output["choices"][0]["message"]["content"])
-            ).group(1)
-        )
+        if self.mode == OperationModes.CHAT.value:
+            return str(
+                cast(
+                    Match,
+                    re.search(
+                        "PATTERN: (.*)\n",
+                        self.output["choices"][0]["message"]["content"],
+                    ),
+                ).group(1)
+            )
+        else:
+            return str(
+                cast(
+                    Match,
+                    re.search("PATTERN: (.*)\n", self.output["choices"][0]["text"]),
+                ).group(1)
+            )
 
     @property
     def entities(self) -> Tuple[str]:
         """Get the predicted entities."""
-        return tuple(
-            cast(
-                Match,
-                re.search("SYMBOLS: (.*)", self.output["choices"][0]["message"]["content"]),
+        if self.mode == OperationModes.CHAT.value:
+            return tuple(
+                cast(
+                    Match,
+                    re.search(
+                        "SYMBOLS: (.*)", self.output["choices"][0]["message"]["content"]
+                    ),
+                )
+                .group(1)
+                .split(", ")
             )
-            .group(1)
-            .split(", ")
-        )
+        else:
+            return tuple(
+                cast(
+                    Match, re.search("SYMBOLS: (.*)", self.output["choices"][0]["text"])
+                )
+                .group(1)
+                .split(", ")
+            )
 
 
-def parse_gpt_output(gpt_output: dict) -> GPTOutput:
+def parse_gpt_output(gpt_output: dict, operation_mode: str) -> GPTOutput:
     """
     Parse the GPT output.
 
     :param gpt_output: the json description of the GPT prediction.
+    :param operation_mode: the operation mode of the GPT engine.
     :return: a GPTOutput instance.
     """
-    wrapper = _GPTOutputWrapper(gpt_output)
+    wrapper = _GPTOutputWrapper(gpt_output, operation_mode)
     pattern: str = wrapper.pattern
     entities: Tuple[str] = wrapper.entities
     gpt_result = GPTOutput(pattern, entities)
